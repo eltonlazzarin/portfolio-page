bcee31044e61fe8db9c9a67d13d355d1c3a9a3b0
Elton Lazzarin <elton.lazzarin@outlook.com>
2020-03-03 21:25:53 -0300

    project file

---
+++ public/scripts.js
@@ -0,0 +1,23 @@
+const modalOverlay = document.querySelector(".modal-overlay");
+const cards = document.querySelectorAll(".card");
+
+for (let card of cards) {
+  card.addEventListener("click", () => {
+    const videoId = card.getAttribute("id");
+    window.location.href = `/video?id=${videoId}`;
+
+    /*
+    modalOverlay.classList.add("active");
+    modalOverlay.querySelector(
+      "iframe"
+    ).src = `https://www.youtube.com/embed/${videoId}`;
+    */
+  });
+}
+
+/*
+document.querySelector(".close-modal").addEventListener("click", () => {
+  modalOverlay.classList.remove("active");
+  modalOverlay.querySelector("iframe").src = "";
+});
+*/
